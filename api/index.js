3b038a63b30a1352d11508a06141d6e0f7e388d0
thienvu1996 <vuthien616@gmail.com>
2026-04-02 10:24:21 +0700

    update

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import jsonServer from 'json-server'
+import path from 'path'
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(process.cwd(), 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+// Rewrite /api/* to /* 
+// Example: /api/properties -> /properties
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}))
+
+server.use(router)
+
+export default server
